ce5e30137758f26fb1291b385002fd7abb752dec
And <pozand47@gmail.com>
2022-11-13 22:35:53 +0100

    Initial commit

--- src/AndersonTest.java
+++ src/AndersonTest.java
@@ -1,21 +1,17 @@
 public class AndersonTest implements Runnable {
     int x = 0, y = 0;
-
     int addX() {
         x++;
         return x;
     }
-
     int addY() {
         y++;
         return y;
     }
-
     public void run(){
         for (int i = 0; i<10; i++)
             System.out.println(Thread.currentThread().getName() + " : "+ addX()+ " : " + addY() );
         }
-
     public static void main(String[] args) {
         AndersonTest obj1 = new AndersonTest();
         AndersonTest obj2 = new AndersonTest();
